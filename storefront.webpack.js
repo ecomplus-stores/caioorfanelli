31a50669bc93bf8db44d186a916ad0181eed4db9
matheusgnreis <matheus@e-com.club>
2023-03-07 02:30:56 -0300

    chore: init file input

---
+++ storefront.webpack.js
@@ -0,0 +1,12 @@
+// storefront.webpack.js
+
+const path = require('path')
+
+module.exports = () => ({
+  resolve: {
+    alias: {
+      './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'),
+      './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js')
+    }
+  }
+})
